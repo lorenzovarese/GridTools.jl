a657e357d525c053b6214568ed6dbadfdf677267
jeffzwe <zjeffrey@student.ethz.ch>
2023-08-07 17:19:42 +0200

    Benchmark Trial

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,13 +4,13 @@ push!(LOAD_PATH, path_to_package)
 using BenchmarkTools
 using GridTools
 
-suite = BenchmarkGroup()
+SUITE = BenchmarkGroup()
 
-suite["arith_broadcast"] = BenchmarkGroup()
+SUITE["arith_broadcast"] = BenchmarkGroup()
 
 a = rand(1000, 1000); b = rand(1000,1000); c = rand(1000,1000)
 af = Field((Cell, K), rand(1000, 1000)); bf = Field((Cell, K), rand(1000, 1000)); cf = Field((Cell, K), rand(1000, 1000))
-suite["arith_broadcast"]["arrays"] = @benchmarkable a .+ b .- c
-suite["arith_broadcast"]["fields"] = @benchmarkable af .+ bf .- cf
+SUITE["arith_broadcast"]["arrays"] = @benchmarkable a .+ b .- c
+SUITE["arith_broadcast"]["fields"] = @benchmarkable af .+ bf .- cf
 
-run(suite, verbose = true, seconds = 1)
+run(SUITE, verbose = true, seconds = 1)
